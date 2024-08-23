9add5466ff27b629a6ab60f225085d55ac18c912
dasith <dasiththisaru@gmail.com>
2024-08-23 10:05:28 +0530

    create service interface and implementation

---
+++ src/main/java/com/ijse/dep/EmployeeCrud/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.ijse.dep.EmployeeCrud.service;
+
+import com.ijse.dep.EmployeeCrud.enity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+}
---
+++ src/main/java/com/ijse/dep/EmployeeCrud/service/ServiceImpl/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ijse.dep.EmployeeCrud.service.ServiceImpl;
+
+import com.ijse.dep.EmployeeCrud.DAO.EmployeeDAO;
+import com.ijse.dep.EmployeeCrud.enity.Employee;
+import com.ijse.dep.EmployeeCrud.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO){
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
