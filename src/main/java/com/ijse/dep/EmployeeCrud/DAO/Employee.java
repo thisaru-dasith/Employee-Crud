f0c7447c2f8e6e2c145874e4a9c2bbfb125859af
dasith <dasiththisaru@gmail.com>
2024-08-23 09:22:09 +0530

    generate Employee DAO entity class

---
+++ src/main/java/com/ijse/dep/EmployeeCrud/DAO/Employee.java
@@ -0,0 +1,36 @@
+package com.ijse.dep.EmployeeCrud.DAO;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Employee {
+
+    //1.initialize fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    //2.generate constructor
+    public Employee(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    //3.getter/setter
+    //toString
+}
